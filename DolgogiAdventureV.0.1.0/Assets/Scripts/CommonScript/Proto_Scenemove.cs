8f0b14763aa8a4d8ceba73790cc84c09d0d9b30e
BaekNothing <king0765@naver.com>
2019-06-25 21:15:18 +0900

    노트북에서의 변경점 업로드
    노트북에서 일어난 변경사항 업로드

---
+++ DolgogiAdventureV.0.1.0/Assets/Scripts/CommonScript/Proto_Scenemove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Proto_Scenemove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    public void Click0()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void Click1()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+
+    public void Click2()
+    {
+        SceneManager.LoadScene(3);
+
+    }
+
+
+}
